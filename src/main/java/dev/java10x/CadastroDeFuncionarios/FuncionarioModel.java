a6408f3940c4c33b8d54899872c2d31d6cdb02a2
theoleal <theojlleal@gmail.com>
2026-04-01 20:32:12 -0300

    Criada Entidade FuncionarioModel

---
+++ src/main/java/dev/java10x/CadastroDeFuncionarios/FuncionarioModel.java
@@ -0,0 +1,48 @@
+package dev.java10x.CadastroDeFuncionarios;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "tb_cadatro")
+public class FuncionarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public FuncionarioModel() {
+    }
+
+    public FuncionarioModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
