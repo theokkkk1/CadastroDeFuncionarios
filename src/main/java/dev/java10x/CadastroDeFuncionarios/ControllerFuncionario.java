68a19590082044fe1a1f489eade5772e65b218f8
theoleal <theojlleal@gmail.com>
2026-04-01 20:32:12 -0300

    Criada Entidade FuncionarioModel

--- src/main/java/dev/java10x/CadastroDeFuncionarios/ControllerFuncionario.java
+++ src/main/java/dev/java10x/CadastroDeFuncionarios/ControllerFuncionario.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class ControllerFuncionario {
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
