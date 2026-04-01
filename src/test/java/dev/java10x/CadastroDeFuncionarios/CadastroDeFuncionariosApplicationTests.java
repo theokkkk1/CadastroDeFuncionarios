405d3c97851112ad55cd41611228c89a53f50859
theoleal <theojlleal@gmail.com>
2026-04-01 18:28:20 -0300

    first commit

---
+++ src/test/java/dev/java10x/CadastroDeFuncionarios/CadastroDeFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeFuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
