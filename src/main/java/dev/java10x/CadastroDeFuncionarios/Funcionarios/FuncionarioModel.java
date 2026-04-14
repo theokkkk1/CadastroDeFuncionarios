2cda772f45a04ce237a7af54629004eae1a6a1a8
theoleal <theojlleal@gmail.com>
2026-04-14 11:36:35 -0300

    modelagem de banco de dados,relacionamento e TarefasModel, FuncionarioModel concluidas

--- src/main/java/dev/java10x/CadastroDeFuncionarios/Funcionarios/FuncionarioModel.java
+++ src/main/java/dev/java10x/CadastroDeFuncionarios/Funcionarios/FuncionarioModel.java
@@ -2,11 +2,15 @@ package dev.java10x.CadastroDeFuncionarios.Funcionarios;
 
 import dev.java10x.CadastroDeFuncionarios.Tarefas.TarefasModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_cadatro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class FuncionarioModel {
     //@ManyToOne UM FUNCIONARIO TEM UMA UNICA TAREFA
     @Id
@@ -20,36 +24,7 @@ public class FuncionarioModel {
     private TarefasModel tarefas;
 
 
-    public FuncionarioModel() {
-    }
 
-    public FuncionarioModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
 
-    public String getNome() {
-        return nome;
-    }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
--- src/main/java/dev/java10x/CadastroDeFuncionarios/Tarefas/TarefasModel.java
+++ src/main/java/dev/java10x/CadastroDeFuncionarios/Tarefas/TarefasModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeFuncionarios.Tarefas;
 
 import dev.java10x.CadastroDeFuncionarios.Funcionarios.FuncionarioModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_tarefa")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TarefasModel {
     // @OneToMany Uma tarefa pode ter varios funcionarios
     @Id
@@ -17,45 +23,8 @@ public class TarefasModel {
     @OneToMany(mappedBy = "tarefas")
     private List<FuncionarioModel> funcionarioModels;
 
-    public TarefasModel() {
-    }
 
-    public TarefasModel(long id, String nome, String dificuldade, List<FuncionarioModel> funcionarioModels) {
-        this.id = id;
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-        this.funcionarioModels = funcionarioModels;
-    }
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public List<FuncionarioModel> getFuncionarioModels() {
-        return funcionarioModels;
-    }
-
-    public void setFuncionarioModels(List<FuncionarioModel> funcionarioModels) {
-        this.funcionarioModels = funcionarioModels;
-    }
 }
+
