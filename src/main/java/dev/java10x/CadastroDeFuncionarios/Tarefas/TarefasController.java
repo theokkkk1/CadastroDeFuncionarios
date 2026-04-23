97eff5d9b1357cff42a7696c63a75ef8a97b9925
theoleal <theojlleal@gmail.com>
2026-04-23 10:05:06 -0300

    Feat: Cria as rotas do Controller com mock

--- src/main/java/dev/java10x/CadastroDeFuncionarios/Tarefas/TarefasController.java
+++ src/main/java/dev/java10x/CadastroDeFuncionarios/Tarefas/TarefasController.java
@@ -1,4 +1,35 @@
 package dev.java10x.CadastroDeFuncionarios.Tarefas;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController //Diz pro Spring: “essa classe responde requisições HTTP”
+@RequestMapping("tarefas") // Indica que tudo vai ser mapeado aqui. Define caminho base ou rota
 public class TarefasController {
+
+    // GET -- Mandar uma requisicao para mostrar as tarefas
+    @GetMapping("/listar")
+    public String listarTarefa(){
+        return "Tarefas listadas com sucesso";
+    }
+    // POST -- Mandar uma requisicao para criar as tarefas
+    @PostMapping("/criar")
+    public String criarTarefa(){
+        return "Tarefa criada com sucesso";
+    }
+
+    // PUT -- Mandar uma requisicao para alterar as tarefas
+    @PutMapping("/alterar")
+    public String alterarTarefa(){
+        return "Tarefa alterada com sucesso";
+    }
+
+    // DELETE -- Mandar uma requisicao para deletar as tarefas
+    @DeleteMapping("/Deletar")
+    public String deletarTarefa(){
+        return "tarefa deletada com sucesso";
+    }
+
+
+
+
 }
