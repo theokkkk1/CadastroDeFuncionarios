e3ae076e7c0f35fe3887bce6c682a0ca56a5347c
theoleal <theojlleal@gmail.com>
2026-04-29 10:38:33 -0300

    feat: inicaliza o repository, e cria os metodos criar funcionario e listar funcionario

--- src/main/java/dev/java10x/CadastroDeFuncionarios/Funcionarios/FuncionarioService.java
+++ src/main/java/dev/java10x/CadastroDeFuncionarios/Funcionarios/FuncionarioService.java
@@ -1,4 +1,28 @@
 package dev.java10x.CadastroDeFuncionarios.Funcionarios;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FuncionarioService {
+
+    private FuncionarioRepository funcionarioRepository;
+
+    public FuncionarioService(FuncionarioRepository funcionarioRepository) {
+        this.funcionarioRepository = funcionarioRepository;
+    }
+
+    // Listar todos os meus Funcionarios
+    public List<FuncionarioModel> listarFuncionarios(){
+        return funcionarioRepository.findAll();
+    }
+
+    // Criar todos os meus Funcionarios
+    public FuncionarioModel criarFuncionario(FuncionarioModel funcionario){
+        return funcionarioRepository.save(funcionario);
+    }
+    //
+
+
 }
