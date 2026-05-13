36eed045b8866ec42b8a90806ab360e4365345e8
theoleal <theojlleal@gmail.com>
2026-05-13 11:45:50 -0300

    feat: adiciona DTO e Mapper para tarefas para tirar responsabilidade total do model

--- src/main/java/dev/java10x/CadastroDeFuncionarios/Tarefas/TarefasDTO.java
+++ src/main/java/dev/java10x/CadastroDeFuncionarios/Tarefas/TarefasDTO.java
@@ -1,4 +1,19 @@
 package dev.java10x.CadastroDeFuncionarios.Tarefas;
 
+import dev.java10x.CadastroDeFuncionarios.Funcionarios.FuncionarioModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TarefasDTO {
+    private Long id;
+    private String nome;
+    private String dificuldade;
+
+
 }
--- src/main/java/dev/java10x/CadastroDeFuncionarios/Tarefas/TarefasMapper.java
+++ src/main/java/dev/java10x/CadastroDeFuncionarios/Tarefas/TarefasMapper.java
@@ -1,4 +1,31 @@
 package dev.java10x.CadastroDeFuncionarios.Tarefas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class TarefasMapper {
+
+    // DTO -> Model
+    public TarefasModel map(TarefasDTO tarefasDTO){
+
+        TarefasModel tarefasModel = new TarefasModel();
+
+        tarefasModel.setId(tarefasDTO.getId());
+        tarefasModel.setNome(tarefasDTO.getNome());
+        tarefasModel.setDificuldade(tarefasDTO.getDificuldade());
+
+        return tarefasModel;
+    }
+
+    // Model -> DTO
+    public TarefasDTO map(TarefasModel tarefasModel){
+
+        TarefasDTO tarefasDTO = new TarefasDTO();
+
+        tarefasDTO.setId(tarefasModel.getId());
+        tarefasDTO.setNome(tarefasModel.getNome());
+        tarefasDTO.setDificuldade(tarefasModel.getDificuldade());
+
+        return tarefasDTO;
+    }
 }
